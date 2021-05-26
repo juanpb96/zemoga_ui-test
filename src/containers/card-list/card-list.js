0f033cb6cfe2e30715a7e7ae2d203dc8e3eb5db7
MasterDev <juanpb.96@gmail.com>
2021-05-25 22:34:27 -0500

    Add click events and handle card data

--- src/containers/card-list/card-list.js
+++ src/containers/card-list/card-list.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './card-list.css';
 import { Card } from '../card/card';
+import Firebase from '../../util/Firebase';
 import moment from 'moment';
 
 
@@ -17,6 +18,11 @@ export const CardList = () => {
             }).then(jsonResponse => setCelebrities(jsonResponse))
             .catch(error => console.error(error));
     }, []);
+    
+    const [view, setView] = useState('Grid');
+
+    // Get dropdown list
+    const cardsDisplay = document.getElementsByClassName('card-list__display')[0];
 
     const calculatePercentage = (votes, totalVotes) => Number((votes / totalVotes) * 100).toFixed(2);
 
@@ -29,6 +35,7 @@ export const CardList = () => {
 
             return <Card 
                         key={i} 
+                        id={i}
                         name={celebrity.name}
                         description={celebrity.description}
                         category={celebrity.category}
@@ -37,20 +44,37 @@ export const CardList = () => {
                         isPositive={isPositive}
                         positiveVotes={positiveVotes}
                         negativeVotes={negativeVotes}
+                        view={view}
+                        onSubmit={handleVote}
                    />
         });
     };
 
+    const handleClick = ({target}) => {
+        setView(() => target.innerText);
+        cardsDisplay.removeAttribute('open');
+    };
+
+    const handleVote = (id, vote) => {
+        const celebrityToUpdate = celebrities.find((celebrity, i) => i === id);
+        celebrityToUpdate.votes[vote] += 1;
+        setCelebrities(prev => [...prev].map((celebrity, i) => i === id ? celebrityToUpdate : celebrity));
+        Firebase.updateData(id, celebrityToUpdate);
+    };
+
     return (
-        <section>
-            <div>
-                <h2>Previous Rulings</h2>
-                <select className="display-type">
-                    <option>List</option>
-                    <option>Grid</option>
-                </select>
+        <section className="card-list">
+            <div className="card-list__header">
+                <h2 className="card-list__title">Previous Rulings</h2>
+                <details className="card-list__display">
+                    <summary>{ view }</summary>
+                    <ul>
+                        <li onClick={handleClick}>Grid</li>
+                        <li onClick={handleClick}>List</li>
+                    </ul>
+                </details>
             </div>
-            <div className="cards-container">
+            <div className="cards-container" aria-label={view}>
                 {listItems()}
             </div>
         </section>
