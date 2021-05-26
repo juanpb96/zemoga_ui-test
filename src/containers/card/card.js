0965221a1afdbfc67553414607fca6c2e9b05942
MasterDev <juanpb.96@gmail.com>
2021-05-25 22:31:51 -0500

    Update structure for list view and add events for voting

--- src/containers/card/card.js
+++ src/containers/card/card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './card.css';
 import thumbsUp from '../../assets/img/thumbs-up.svg'; 
 import thumbsDown from '../../assets/img/thumbs-down.svg';
@@ -13,8 +13,26 @@ const negativeStatus = {
 };
 
 export const Card = (props) => {
+    const [vote, setVote] = useState(null);
+    const [isSubmited, setIsSubmited] = useState(false);
+
+    const handleClick = (val) => {
+        setVote(val);
+    };
+
+    const handleSubmit = () => {
+        if (vote === null) {
+            setIsSubmited(false)
+            return;
+        }
+        props.onSubmit(props.id, vote);
+        setIsSubmited(true);
+        setVote(null);
+    };
+
+
     return (
-        <article className="card">
+        <article className="card" aria-label={props.view}>
             <img 
                 className="card__background"
                 src={ props.picture }
@@ -28,28 +46,32 @@ export const Card = (props) => {
                         alt=""
                     />
                 </div>
-                <div className="card__header">
-                    <h3 className="card__title text-overflow">{ props.name }</h3>
-                </div>
-                <p className="card__description text-overflow">{ props.description }</p>
-                <div className="card__vote">
-                    <h4 className="card__last-updated">{ props.lastUpdated } in { props.category }</h4>
-                    <div className="card__buttons">
-                        <button className="button-option" aria-label="thumbs up">
-                            <img 
-                                className="icon-button"
-                                src={ thumbsUp }
-                                alt="Vote Positive"
-                            />
-                        </button>
-                        <button className="button-option" aria-label="thumbs down">
-                            <img 
-                                className="icon-button"
-                                src={ thumbsDown }
-                                alt="Vote Negative"
-                            />
-                        </button>
-                        <button className="button-vote">Vote Now</button>
+                <div className="card__row">
+                    <div className="card__col">
+                        <div className="card__header">
+                            <h3 className="card__title text-overflow">{ props.name }</h3>
+                        </div>
+                        <p className="card__description text-overflow">{ props.description }</p>
+                    </div>
+                    <div className="card__vote">
+                        <h4 className="card__last-updated">{ isSubmited ? 'Thank you for voting!' : `${props.lastUpdated} in ${props.category}`}</h4>
+                        <div className="card__buttons">
+                            <button className="button-option" style={ isSubmited ? { display: 'none' } : { display: 'flex' } } aria-label="thumbs up" onClick={() => handleClick('positive')}>
+                                <img 
+                                    className="icon-button"
+                                    src={ thumbsUp }
+                                    alt="Vote Positive"
+                                />
+                            </button>
+                            <button className="button-option" style={ isSubmited ? { display: 'none' } : { display: 'flex' } } aria-label="thumbs down" onClick={() => handleClick('negative')}>
+                                <img 
+                                    className="icon-button"
+                                    src={ thumbsDown }
+                                    alt="Vote Negative"
+                                />
+                            </button>
+                            <button className="button-vote" onClick={handleSubmit}>{ isSubmited ? 'Vote Again' : 'Vote Now'}</button>
+                        </div>
                     </div>
                 </div>
                 <div className="gauge-bar">
